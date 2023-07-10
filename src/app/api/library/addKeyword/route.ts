2d8891fede6b4e3f3e208f63e3738c5b29b68d3f
Chun-Yu Lo (Andy Lo) <chunyulo@arch.nctu.edu.tw>
2023-07-10 10:50:00 +0800

    fix(addkeyword): delete the code related to firestore to test the project

--- src/app/api/library/addKeyword/route.ts
+++ src/app/api/library/addKeyword/route.ts
@@ -5,19 +5,25 @@ import { db } from "@/app/utils/firebase";
 export async function POST(req: Request) {
   const requestBody = await req.json(); // 解析請求內容為 JSON
   const { keywordToAdd } = requestBody;
-  console.log(keywordToAdd);
+  console.log(`myKeyWord: ${keywordToAdd}`);
+  console.log("=====!!!=====");
   const userUID = "9TjkIyfzR6VmZyrBLX9f9348nni1";
   const selectedMap = "map3";
-  console.log("=====!!!=====");
-  const docRef = doc(db, "users", userUID, "maps", selectedMap);
-  const docSnap = await getDoc(docRef);
-  if (docSnap.exists()) {
-    const keywords = docSnap.data()?.keywords;
-    const newKeywords = [...keywords, keywordToAdd];
-    await updateDoc(docRef, { library: newKeywords });
-    console.log("Updated!!!");
-  } else {
-    console.log("DocSnap doesn't exist");
-  }
+  // try {
+  //   const docRef = doc(db, "users", userUID, "maps", selectedMap);
+  //   const docSnap = await getDoc(docRef);
+  //   if (docSnap.exists()) {
+  //     console.log(`docData: ${docSnap.data()}`);
+  //     const keywords = docSnap.data()?.keywords;
+  //     const newKeywords = [...keywords, keywordToAdd];
+  //     await updateDoc(docRef, { library: newKeywords });
+  //     console.log("Updated!!!");
+  //   } else {
+  //     console.log("DocSnap doesn't exist");
+  //   }
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
   return new NextResponse("Keyword is added");
 }
